821cba4db177e037ee15c935cdaf05c933fc0819
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-10 12:17:24 +0530

    finish

---
+++ README.c
@@ -0,0 +1,13 @@
+# dictionary
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+char name[50]={"author"};
+int pages;
+printf("enter the author name");
+sacnf("%c",&author);
+printf("enter the page");
+scanf("%d",&page);
+getch();
+}
